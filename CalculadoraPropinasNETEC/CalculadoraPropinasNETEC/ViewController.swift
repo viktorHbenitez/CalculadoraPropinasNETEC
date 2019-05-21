80d37fb82280c8196d4246a035cd6a3fbea1bf39
Victor Hugo Benitez Bosques <victorHbenitez85@hotmail.com>
2019-05-21 09:14:32 -0500

    first commit

---
+++ CalculadoraPropinasNETEC/CalculadoraPropinasNETEC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculadoraPropinasNETEC
+//
+//  Created by Victor Hugo Benitez Bosques on 21/05/19.
+//  Copyright © 2019 Victor Hugo Benitez Bosques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
